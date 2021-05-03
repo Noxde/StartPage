ac5170fc70f9888b2de04cd1299dba9f56a80d4e
m5tfi <72708423+m5tfi@users.noreply.github.com>
2021-05-03 02:20:18 +0200

    change the icon request url to ours

--- start_page/script.js
+++ start_page/script.js
@@ -93,7 +93,7 @@ function bookmark_onRemoved_listener(id, remove_info) {
 }
 
 function get_favicon_url(bookmark_url) {
-    const best_icon_start = "https://besticon-demo.herokuapp.com/icon?url="
+    const best_icon_start = "https://start-page-browser-extension.herokuapp.com/icon?url="
     const best_icon_size = "&size=48..150..196"
     let url = new URL(bookmark_url)
     url = best_icon_start + url.host + best_icon_size
